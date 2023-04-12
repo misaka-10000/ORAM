ee046244f659260a03a192bccae7e7f3d777d49d
zyh <915792670@qq.com>
2023-04-12 08:28:06 +0800

    modify the main program

--- PathORAM_main.cpp
+++ PathORAM_main.cpp
@@ -17,7 +17,7 @@ int main() {
     // std::streambuf* p=std::cout.rdbuf(fout.rdbuf());
     mongo::client::initialize();
     srand((uint32_t)time(NULL));
-    uint32_t N = 1023;
+    uint32_t N = 15;
     PathORAM* oram = new PathORAM(N);
     //基本上填满的initialization
     for(uint32_t i = 0; i < N*PathORAM_Z; i ++) {
@@ -36,27 +36,30 @@ int main() {
         oram->put(key, value);
     }
     //printf("occupation rate of block close to leaf:%lf\n",oram->getcnt()/(N*PathORAM_Z));
-    int32_t request_num=0;
-    while(!oram->IsEmpty()||request_num<N*PathORAM_Z){
-        while(oram->IsAvailable()&&request_num<N*PathORAM_Z){
-            char str[12];
-            uint32_t blockID = Util::rand_int(N*PathORAM_Z);
-            sprintf(str, "%zu\n",(size_t)blockID);
-            std::string key(str);
-            std::string value;
-            const uint32_t tmp_len = B - AES::BLOCKSIZE - 2 * sizeof(uint32_t);
-            byte tmp_buffer[tmp_len];
-            AutoSeededRandomPool prng;
-            prng.GenerateBlock(tmp_buffer, tmp_len);
-            value = std::string((const char *)tmp_buffer, tmp_len);
-            std::string bID = std::string((const char *)(& blockID), sizeof(uint32_t));
-            value = bID + value;
-            Request r(blockID,value);
-            oram->addRequest(r);
-            request_num++;
+    for(int turn=0;turn<100;turn++){
+        int32_t request_num=0;
+        while(!oram->IsEmpty()||request_num<N*PathORAM_Z){
+            while(oram->IsAvailable()&&request_num<N*PathORAM_Z){
+                char str[12];
+                uint32_t blockID = Util::rand_int(N*PathORAM_Z);
+                sprintf(str, "%zu\n",(size_t)blockID);
+                std::string key(str);
+                std::string value;
+                const uint32_t tmp_len = B - AES::BLOCKSIZE - 2 * sizeof(uint32_t);
+                byte tmp_buffer[tmp_len];
+                AutoSeededRandomPool prng;
+                prng.GenerateBlock(tmp_buffer, tmp_len);
+                value = std::string((const char *)tmp_buffer, tmp_len);
+                std::string bID = std::string((const char *)(& blockID), sizeof(uint32_t));
+                value = bID + value;
+                Request r(blockID,value);
+                oram->addRequest(r);
+                request_num++;
+            }
+            oram->schedule();
         }
-        oram->schedule();
     }
+
     //oram->display();
     std::cout<<"fusion cnt"<<oram->fusion_cnt<<"   "<<"ordinary cnt:"<<oram->ori_cnt<<std::endl;
     delete oram;
