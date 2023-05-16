70a5dd55862eedcadede329823ac92b1ee30303d
zyh <915792670@qq.com>
2023-05-16 23:28:58 +0800

    M

--- PathORAM_main.cpp
+++ PathORAM_main.cpp
@@ -33,30 +33,29 @@ int main() {
         value = bID + value;
         oram->put(key, value);
     }
-    //printf("occupation rate of block close to leaf:%lf\n",oram->getcnt()/(N*PathORAM_Z));
-        int32_t request_num=0;
-        while(!oram->IsEmpty()||request_num<1000){
-            while(oram->IsAvailable()&&request_num<1000){
-                char str[12];
-                uint32_t blockID = Util::rand_int(N*PathORAM_Z);
-                sprintf(str, "%zu\n",(size_t)blockID);
-                std::string key(str);
-                std::string value;
-                const uint32_t tmp_len = B - AES::BLOCKSIZE - 2 * sizeof(uint32_t);
-                byte tmp_buffer[tmp_len];
-                AutoSeededRandomPool prng;
-                prng.GenerateBlock(tmp_buffer, tmp_len);
-                value = std::string((const char *)tmp_buffer, tmp_len);
-                std::string bID = std::string((const char *)(& blockID), sizeof(uint32_t));
-                value = bID + value;
-                Request r(blockID,value);
-                oram->addRequest(r);
-                request_num++;
-            }
-            oram->schedule();
-        }
     //oram->display();
-    std::cout<<"fusion cnt"<<oram->fusion_cnt<<"   "<<"ordinary cnt:"<<oram->ori_cnt<<std::endl;
+    int32_t request_num=0;
+    while(!oram->IsEmpty()||request_num<50000){
+        while(oram->IsAvailable()&&request_num<50000){
+            char str[12];
+            uint32_t blockID = Util::rand_int(N*PathORAM_Z);
+            sprintf(str, "%zu\n",(size_t)blockID);
+            std::string key(str);
+            std::string value;
+            const uint32_t tmp_len = B - AES::BLOCKSIZE - 2 * sizeof(uint32_t);
+            byte tmp_buffer[tmp_len];
+            AutoSeededRandomPool prng;
+            prng.GenerateBlock(tmp_buffer, tmp_len);
+            value = std::string((const char *)tmp_buffer, tmp_len);
+            std::string bID = std::string((const char *)(& blockID), sizeof(uint32_t));
+            value = bID + value;
+            Request r(blockID,value);
+            oram->addRequest(r);
+            request_num++;
+        }
+        oram->schedule();
+    }
+    std::cout<<oram->hit<<std::endl;
     delete oram;
     mongo::client::shutdown();
     return 0;
